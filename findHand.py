6f63e6adaf9d232290d5e7a474e60779ef5e67a5
SinclairHudson <hudson.sinclair00@gmail.com>
2019-03-10 13:15:02 -0400

    Started Training Model

--- findHand.py
+++ findHand.py
@@ -1,22 +1,33 @@
 import tflearn
 import datautil
 import tensorflow as tf
+import cv2
+import numpy as np
 
-testX, testY = datautil.getBatchRange(64)
-print(testX.shape)
-print(testY.shape)
+batch_size = 64
+testX, testY = datautil.getBatchRange(batch_size)
 
 network = tflearn.input_data([None, 280, 120, 3], name='input')
 network = tflearn.conv_2d(network, 3, 5, activation='leaky_relu')
-network = tflearn.conv_2d(network, 3, 20, activation='leaky_relu')
+network = tflearn.conv_2d(network, 3, 20,strides=[1,5,5,1], activation='leaky_relu')
 network = tflearn.fully_connected(network, 336, activation='leaky_relu')
-network = tflearn.reshape(network, (64, 12, 28), name="output")
-network = tflearn.regression(network, optimizer='adam', learning_rate=0.01,
+network = tflearn.reshape(network, (-1, 12, 28), name="output")
+image = tf.reshape(network[:1], [-1, 12, 28, 1])
+tf.summary.image("output", image)
+network = tflearn.regression(network, optimizer='adam', learning_rate=0.05, # <--impatient
                      loss='categorical_crossentropy', name='target')
 
-model = tflearn.DNN(network, tensorboard_verbose=3)
-for x in range(100):
-    X, Y = datautil.getBatchRange(64)
+model = tflearn.DNN(network, tensorboard_verbose=3, max_checkpoints=3)
+print("Loading Model")
+model.load("current.model")
+for x in range(500):
+    X, Y = datautil.getBatchRange(batch_size)
     model.fit({'input': X}, {'target': Y}, n_epoch=32,
               validation_set=({'input': testX}, {'target': testY}),
-              snapshot_step=100, show_metric=True, run_id='convnet')
+              snapshot_step=100, show_metric=True)
+    model.save("current.model")
+
+    image = cv2.imread("./SmallData/" + str(400) + ".jpg")
+    image = np.reshape(image, (1, 280, 120, 3))
+    out = model.predict(image)[0]
+    cv2.imwrite("out.jpg", out)
