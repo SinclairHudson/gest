More Data, but there's still something wrong with the network. Still only averaging 10% training accuracy.

Labeller.py
@@ -1,20 +1,14 @@
 from tkinter import *
-from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
-import tkinter.simpledialog
+import numpy as np
+import cv2
+import skimage.measure
 
-for x in range(2000):
-    coordinates = []
-    xcord = -1
-    ycord = -1
-    xcord2 = -1
-    ycord2 = -1
-
+start = 758
+x = start
+for i in range(2000-start):
+    label = np.zeros((120, 280), dtype=np.uint8)
     root = Tk()
-
-    def continuer():
-        print("C")
-
     #setting up a tkinter canvas
     w = Canvas(root, width=1120, height=480)
 
@@ -23,16 +17,18 @@ for x in range(2000):
     original = original.resize((1120,480)) #resize image
     img = ImageTk.PhotoImage(original)
     w.create_image(0, 0, image=img, anchor="nw")
-    b = Button(w, text="Submit", command=continuer)
     w.pack()
 
-    def printcoords(event):
+    def writecoords(event):
         # outputting x and y coords to console
-        coordinates.append([event.x, event.y])
-        print(event.x, event.y)
+        label[event.y//4][event.x//4] = 255 # white pixel where hand is
+        print("Hand at "+str(event.y//4)+" "+str(event.x//4))
 
-    w.bind("<Button 1>",printcoords)
-
+    w.bind("<Button 1>",writecoords)
 
     root.mainloop()
-    print(coordinates)
+    cv2.imwrite("Labels/"+str(x)+".jpg", label)
+    tiny = skimage.measure.block_reduce(label, (10, 10), np.max)
+    cv2.imwrite("./TinyLabels/" + str(x) + ".jpg", tiny)
+    print("Labelled image "+str(x))
+    x = x + 1

findHand.py
@@ -4,14 +4,14 @@ import tensorflow as tf
 import cv2
 import numpy as np
 
+#something's wrong. Still averaging 10% accuracy on a pretty simple task...
 batch_size = 64
 testX, testY = datautil.getBatchRange(batch_size)
 
-network = tflearn.input_data([None, 280, 120, 3], name='input')
-network = tflearn.conv_2d(network, 3, 5, activation='leaky_relu')
-network = tflearn.conv_2d(network, 3, 20,strides=[1,5,5,1], activation='leaky_relu')
+network = tflearn.input_data([None, 120, 280, 3], name='input')
+network = tflearn.conv_2d(network, 3, 5, activation='leaky_relu', name="small_CONV")
+network = tflearn.conv_2d(network, 3, 20,strides=[1,5,5,1], activation='leaky_relu',name="large_CONV")
 network = tflearn.fully_connected(network, 336, activation='leaky_relu')
-network = tflearn.dropout(network, 0.8) # kill 20 percent of neurons
 network = tflearn.reshape(network, (-1, 12, 28), name="output") #infer the batch size
 image = tf.reshape(network[:1], [-1, 12, 28, 1])
 tf.summary.image("output", image)
@@ -29,6 +29,6 @@ for x in range(500):
     model.save("current.model")
 
     image = cv2.imread("./SmallData/" + str(400) + ".jpg")
-    image = np.reshape(image, (1, 280, 120, 3))
+    image = np.reshape(image, (1, 120, 280, 3))
     out = model.predict(image)[0]
     cv2.imwrite("out.jpg", out)